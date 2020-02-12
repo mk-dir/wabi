88d28658dccd056e01d537548bb451cf66b9614a
GuyFawkes <calebkibut@gmail.com>
2020-02-13 00:58:43 +0300

    segunda

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ posts=[
 'Title':'I am Legendess',
 'Date_posted':'21 October 2018'}]
 
-@app.route("/graph")
+@app.route("/")
 
 def helloworld():
     # # postgres://zrgirmyqlrnepr:80841ec233ebb6b65f7249a8c6bb7402649dbaf0c5e7415d3a449eef2c89c479@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d1eihvigupehq7
