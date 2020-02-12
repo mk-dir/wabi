29478d3bc5b537054556a35d9b9cf49a0ceb8306
GuyFawkes <calebkibut@gmail.com>
2020-02-12 20:11:43 +0300

    second

--- gsk.py
+++
@@ -1,8 +0,0 @@
-posts=[{'Author':'Cullen',
-'Title':'I am Legend',
-'Date_posted':'21 April 2018'},{'Author':'Nkiro',
-'Title':'I am Legendess',
-'Date_posted':'21 October 2018'}]
-
-for post in posts:
-    print(post['Title'])
--- main.py
+++ main.py
@@ -16,13 +16,13 @@ posts=[
 @app.route("/graph")
 
 def helloworld():
+    # # postgres://zrgirmyqlrnepr:80841ec233ebb6b65f7249a8c6bb7402649dbaf0c5e7415d3a449eef2c89c479@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d1eihvigupehq7
+    # conn=psycopg2.connect("dbname='d1eihvigupehq7' user='zrgirmyqlrnepr' host='ec2-54-195-247-108.eu-west-1.compute.amazonaws.com' password='80841ec233ebb6b65f7249a8c6bb7402649dbaf0c5e7415d3a449eef2c89c479'")
 
-    conn=psycopg2.connect("dbname=public user='postgres' host='localhost' password='Pa$$w2rd'")
+    # cur=conn.cursor()
+    # cur.execute("Enter SQL query HERe")
 
-    cur=conn.cursor()
-    cur.execute("Enter SQL query HERe")
-
-    records = cur.fetchall()
+    # records = cur.fetchall()
 
     graph=pygal.Line()
     graph.title='Language Popularity'
